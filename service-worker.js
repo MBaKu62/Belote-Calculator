3329ebba7c1b241626cb92c16f6560976e0075ee
MBaKu62 <l.c.chavatte@gmail.com>
2026-05-28 22:48:32 +0200

    Force SW skipWaiting and claim clients - bust old cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
-// Service worker disabled - clearing all caches
+// Self-destructing service worker
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(names => Promise.all(names.map(name => caches.delete(name))))
@@ -6,6 +10,4 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  // No caching, passthrough only
-});
+self.addEventListener('fetch', event => {});
