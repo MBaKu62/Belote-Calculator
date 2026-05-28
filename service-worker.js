4349a682fb79569709f8c2e001fb1dca2da1c99c
MBaKu62 <l.c.chavatte@gmail.com>
2026-05-28 23:44:18 +0200

    Fix SW: cache fonctionnel avec nettoyage des anciens caches v3

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,31 @@
-// Self-destructing service worker
+const CACHE_NAME = 'belote-cache-v3';
+const URLS_TO_CACHE = [
+  '/Belote-Calculator/',
+  '/Belote-Calculator/index.html',
+  '/Belote-Calculator/manifest.json',
+  '/Belote-Calculator/icon-192.png',
+  '/Belote-Calculator/icon-512.png'
+];
+
 self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(names => Promise.all(names.map(name => caches.delete(name))))
+    caches.keys().then(names =>
+      Promise.all(
+        names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
   );
-  return self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
